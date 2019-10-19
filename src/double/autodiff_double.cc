5f623c012d11c0f33ccab281a4548cad8874ab04
Michael Qian <michaelq94@gmail.com>
2019-10-19 14:42:00 -0700

    Removing commented-out code

--- src/double/autodiff_double.cc
+++ src/double/autodiff_double.cc
@@ -27,22 +27,5 @@ const DualFunc<double> cos(dual_func<double>(std_cos, [](double t) { return -std
 const DualFunc<double> sinh(dual_func<double>(std_sinh, std_cosh));
 const DualFunc<double> cosh(dual_func<double>(std_cosh, std_sinh));
 
-/*
-Dual<double> exp(Dual<double> t) {
-  return Dual(::exp(t.real()), ::exp(t.dual()));
-}
-
-Dual<double> ln(Dual<double> t) {
-  return Dual(::log(t.real()), static_cast<double>(1) / t.dual());
-}
-
-Dual<double> sin(Dual<double> t) {
-  return Dual(::sin(t.real()), ::cos(t.dual()));
-}
-
-Dual<double> cos(Dual<double> t) {
-  return Dual(::cos(t.real()), -(::sin(t.dual())));
-}*/
-
 } // namespace double
 } // namespace autodiff
